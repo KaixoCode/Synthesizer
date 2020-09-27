02170b01d2e42e0d509b068a1717066b29a09fb5
KaixoCode <33551498+KaixoCode@users.noreply.github.com>
2020-09-27 13:47:15 +0200

    Update ADSR.hpp

--- src/components/envelopes/ADSR.hpp
+++ src/components/envelopes/ADSR.hpp
@@ -12,9 +12,11 @@ public:
     double S = 1.0; // (0-1)
     double R = 0.1; // s
 
-    double AC = 1.0; // curve
-    double DC = 1.0; // curve
-    double RC = 1.0; // curve
+    double AC = 0.5;
+    double DC = 0.5;
+    double RC = 0.5;
+
+    ADSR(double a, double d, double s, double r) : A(a), D(d), S(s), R(r) {};
 
     Sample NextSample() override;
     Sample GetSample() override;
