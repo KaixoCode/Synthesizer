baddb1f032e3c4a8ea602b0143c11db61f171dc1
KaixoCode <33551498+KaixoCode@users.noreply.github.com>
2020-09-28 00:48:38 +0200

    Update Utils.hpp

--- src/utils/Utils.hpp
+++ src/utils/Utils.hpp
@@ -1,8 +1,8 @@
 #define TWO_PI 6.28318530718
 #define PI (TWO_PI/2.0)
 
-#define SAMPLE_RATE (44100 * 8)
+#define SAMPLE_RATE (44100 * 2)
 #define BUFFER_SIZE 512
-#define CHANNELS 1
+#define CHANNELS 2
 
 typedef double Sample;
