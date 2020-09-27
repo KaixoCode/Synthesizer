28e22b74519670d5f37ea033ce05f9bd55c4b004
KaixoCode <33551498+KaixoCode@users.noreply.github.com>
2020-09-27 15:09:59 +0200

    Separated different filter types

--- src/components/filters/Filter.hpp
+++ src/components/filters/Filter.hpp
@@ -1,15 +1,17 @@
+#pragma once
 #include "../Component.hpp"
 #include "../../utils/Utils.hpp"
 
 class Filter : public Component
 {
-
+protected:
     Sample x[3];
     Sample y[3];
-public:
+
     double cutoff = 1000;
 
+public:
+    Filter& Cutoff(double);
     Sample Apply(Sample a);
-    Sample NextSample() override;
     Sample GetSample() override;
 };
