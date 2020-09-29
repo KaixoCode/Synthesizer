b684074ed03d9e235a88748149af8f633b4281ae
KaixoCode <33551498+KaixoCode@users.noreply.github.com>
2020-09-29 12:29:14 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "components/envelopes/ADSR.hpp"
 #include "components/filters/LPF.hpp"
 #include "components/filters/HPF.hpp"
-#include "components/oscillators/Oscillator.hpp"
+#include "components/generators/Oscillator.hpp"
 #include "components/effects/Delay.hpp"
 #include "components/effects/Flanger.hpp"
 #include "components/effects/Chorus.hpp"
@@ -71,9 +71,9 @@ int main(void) {
     reverb1.Offset(0.0032);
     reverb2.Offset(0.0038);
 
-    osc.waveTable = new Wavetables::Saw;
+    osc.waveTable = new Wavetables::Triangle;
     lfo.waveTable = new Wavetables::Triangle;
-    fmosc.waveTable = new Wavetables::Sine;
+    fmosc.waveTable = new Wavetables::Triangle;
 
     kick.waveTable = new Wavetables::Sine;
     kick.Frequency(60);
@@ -93,9 +93,9 @@ int main(void) {
 bool trig = false;
 void AudioCallback(float* buffer)
 {
-    if (gpio[31] != 88) 
+    if (gpio[31] != 1) 
     { 
-        int note = Midi::NoteToScale(gpio[31], new int[7]{ 0, 2, 3, 5, 7, 8, 10 }, 7);
+        int note = Midi::NoteToScale(gpio[31] * 64 + 24, new int[7]{ 0, 2, 3, 5, 7, 8, 10 }, 7);
         if (!trig) MidiPress(note, 1);
         osc.Frequency(Midi::NoteToFreq(note));
         trig = true;
@@ -109,22 +109,47 @@ void AudioCallback(float* buffer)
     for (int i = 0; i < CHANNELS * BUFFER_SIZE;)
     {
         // Mono pre-effect channel
-        Sample pre = (
-            lpf2.Cutoff(20000) >> ( // Filter everything above 20kHz away
-                env.Attack(gpio[16]).Decay(gpio[17]).Sustain(gpio[18]).Release(gpio[19]) >> ( // Apply the envelope
-                    lpf.Cutoff(++env2.Attack(gpio[20]).Decay(gpio[21]).Sustain(gpio[22]).Release(gpio[23]) * 20000 + 1000) >> (// Lowpass filter whose cutoff is modulated by env2.
-                        ++osc.FM(++fmosc.Frequency(osc.Frequency() * gpio[1]), gpio[0]))))); // Osc is FMed by fmosc
+        Sample pre = 
+            ++osc                       // Oscillator
+            .FM(gpio[0] * 20000.0 *         // FMed by fmosc
+                ++fmosc                 // Frequency of fmosc dependent on osc.
+                .Frequency(gpio[1] * 8 * osc.Frequency()))
+            >> lpf                      // Running through a lowpass filter
+            .Cutoff(20000 *      // Cutoff dependend on env2
+                ++env2                  
+                .Attack(gpio[20])       // Params modulated by gpio values
+                .Decay(gpio[21])
+                .Sustain(gpio[22])
+                .Release(gpio[23]))
+            >> env                      // Running through an envelope
+            .Attack(gpio[16])           // Params modulated by gpio values
+            .Decay(gpio[17])
+            .Sustain(gpio[18])
+            .Release(gpio[19])
+            >> lpf2.Cutoff(20000);      // Running through a lowpass filter
 
         // Left stereo channel
-        Sample post1 = 0.5 * (
-            delay1.Mix(gpio[2]).Time(0.300).Feedback(0.4) >> ( // Delay effect
-                chorus1.Intensity(0.5).Frequency(0.20).Feedback(0.1) >> pre)); // Chorus effect
-           
+        Sample post1 = 0.5 * pre        // Post processing on pre
+            >> chorus1                  // Running through chorus
+            .Intensity(0.5)
+            .Frequency(0.20)
+            .Feedback(0.1)
+            >> delay1                   // Running through delay
+            .Mix(gpio[2])               // Mix modulated by gpio value 
+            .Time(0.300)
+            .Feedback(0.4);
+
         // Right stereo channel
-        Sample post2 = 0.5 * (
-            delay2.Mix(gpio[2]).Time(0.320).Feedback(0.4) >> ( // Delay effect
-                chorus2.Intensity(0.5).Frequency(0.30).Feedback(0.1) >> pre)); // Chorus effect
-             
+        Sample post2 = 0.5 * pre        // Post processing on pre
+            >> chorus2                  // Running through chorus
+            .Intensity(0.5)
+            .Frequency(0.30)
+            .Feedback(0.1)
+            >> delay2                   // Running through delay
+            .Mix(gpio[2])               // Mix modulated by gpio value 
+            .Time(0.320)
+            .Feedback(0.4);
+
         // Limit value between -1 and 1 to prevent clipping.
         buffer[i++] = post1 > 1 ? 1 : post1 < -1 ? -1 : post1;
         buffer[i++] = post2 > 1 ? 1 : post2 < -1 ? -1 : post2;
