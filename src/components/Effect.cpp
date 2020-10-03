633711db6b8ff7eddb3bd4281a6917a60ed6ebfa
KaixoCode <33551498+KaixoCode@users.noreply.github.com>
2020-10-03 19:58:13 +0200

    Update Effect.cpp

--- src/components/Effect.cpp
+++ src/components/Effect.cpp
@@ -6,13 +6,13 @@ Sample operator>>(Sample b, Effect& a)
     return a.Apply(b);
 }
 
-Stereo operator>>(Sample b, StereoEffect& a)
+Stereo operator>>(Sample b, const StereoEffect& a)
 {
     Stereo ef{ a.left.Apply(b), a.right.Apply(b) };
     return ef;
 }
 
-Stereo operator>>(Stereo& b, StereoEffect& a)
+Stereo operator>>(Stereo& b, const StereoEffect& a)
 {
     Stereo ef{ a.left.Apply(b.left), a.right.Apply(b.right) };
     return ef;
