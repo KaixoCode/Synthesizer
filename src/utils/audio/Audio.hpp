e35b4f2405e620e146d002c767b2d387f316ddbd
KaixoCode <33551498+KaixoCode@users.noreply.github.com>
2020-10-04 18:49:14 +0200

    Update Audio.hpp

--- src/utils/audio/Audio.hpp
+++ src/utils/audio/Audio.hpp
@@ -1,14 +1,14 @@
 #pragma once
 #define OVERSAMPLING 1
-#define SAMPLE_RATE (48000 * OVERSAMPLING)
+#define SAMPLE_RATE (44100 * OVERSAMPLING)
 #define BUFFER_SIZE 512
-#define CHANNELS 1
+#define CHANNELS 2
 
 #include <functional>
 #include <array>
 typedef double Sample;
 
-typedef std::array<float, CHANNELS* BUFFER_SIZE> Buffer;
+typedef std::array<float, CHANNELS * BUFFER_SIZE> Buffer;
 
 struct Stereo
 {
